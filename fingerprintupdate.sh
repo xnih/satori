0cd914ec6dc982e32d12a4932a3bad2871342b6a
Eric <xnih13@gmail.com>
2018-11-12 18:14:47 -0800

    Delete fingerprintupdate.sh
    moving to full git implementation

--- fingerprintupdate.sh
+++
@@ -1,5 +0,0 @@
-wget chatteronthewire.org/download/updates/satori/fingerprints/tcp.xml -O tcp.xml
-wget chatteronthewire.org/download/updates/satori/fingerprints/dhcp.xml -O dhcp.xml
-wget chatteronthewire.org/download/updates/satori/fingerprints/webuseragent.xml -O webuseragent.xml
-
-
