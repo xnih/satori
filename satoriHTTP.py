fa17ec559b3c4fbd2fb76f186c1d4839debd44dd
Eric <xnih13@gmail.com>
2018-11-23 11:29:37 -0700

    Add files via upload
    added Web Server fingerprinting into the http module

--- satoriHTTP.py
+++ satoriHTTP.py
@@ -12,6 +12,45 @@ from pypacker.layer3 import ip
 # cat output.txt | awk -F';' '{print $3, $4, $5, $6, $7}' | sort -u > output2.txt
 #
 
+
+def httpServerProcess(eth, ts, serverExactList, serverPartialList):
+  ip4 = eth.upper_layer
+  tcp1 = eth.upper_layer.upper_layer
+  http1 = eth.upper_layer.upper_layer.upper_layer
+
+  timeStamp = datetime.utcfromtimestamp(ts).isoformat()
+  hdrServer = ''
+  bodyServer = ''
+
+  try:
+    if (http1.hdr != None) and (http1.hdr):
+      hdr = dict(http1.hdr)
+      hdrServer = hdr[b'Server'].decode("utf-8", "strict")
+    if (http1.body_bytes):
+      body = http1.body_bytes.decode("utf-8", "strict")
+      i = body.find("Server: ") 
+      if i > 1:
+        v = body[i:]
+        i = v.find("\n")
+        v = v[:i]
+        i = v.find(":")
+        bodyServer = v[i+1:].strip()
+  except:
+    pass
+
+  if (hdrServer != ''):
+    httpServerFingerprint = httpServerFingerprintLookup(serverExactList, serverPartialList, hdrServer)
+    #not ideal but converting any ; to | for parsing reasons!
+#    changedUserAgent = hdrUserAgent.replace(';', '|')
+    print("%s;%s;%s;HTTPSERVER;%s;%s" % (timeStamp, eth[ip.IP].src_s, eth[ethernet.Ethernet].src_s, hdrServer, httpServerFingerprint))
+  if (bodyServer != ''):
+    httpServerFingerprint = httpServerFingerprintLookup(serverExactList, serverPartialList, bodyServer)
+    #not ideal but converting any ; to | for parsing reasons!
+#    changedUserAgent = bodyUserAgent.replace(';', '|')
+    print("%s;%s;%s;HTTPSERVER;%s;%s" % (timeStamp,eth[ethernet.Ethernet].src_s, eth[ip.IP].src_s, bodyServer, httpServerFingerprint))
+
+
+
 def httpUserAgentProcess(eth, ts, useragentExactList, useragentPartialList):
   ip4 = eth.upper_layer
   tcp1 = eth.upper_layer.upper_layer
@@ -27,7 +66,7 @@ def httpUserAgentProcess(eth, ts, useragentExactList, useragentPartialList):
       hdrUserAgent = hdr[b'User-Agent'].decode("utf-8", "strict")
     if (http1.body_bytes):
       body = http1.body_bytes.decode("utf-8", "strict")
-      i = body.find("User-Agent: ") 
+      i = body.find("User-Agent: ")
       if i > 1:
         v = body[i:]
         i = v.find("\n")
@@ -47,6 +86,40 @@ def httpUserAgentProcess(eth, ts, useragentExactList, useragentPartialList):
     #not ideal but converting any ; to | for parsing reasons!
     changedUserAgent = bodyUserAgent.replace(';', '|')
     print("%s;%s;%s;USERAGENT;%s;%s" % (timeStamp,eth[ethernet.Ethernet].src_s, eth[ip.IP].src_s, changedUserAgent, httpUserAgentFingerprint))
+
+
+def BuildHTTPServerFingerprintFiles():
+  # converting from the xml format to a more flat format that will hopefully be faster than walking the entire xml every FP lookup
+  serverExactList = {}
+  serverPartialList = {}
+
+  obj = untangle.parse('fingerprints/web.xml')
+  fingerprintsCount = len(obj.WEBSERVER.fingerprints)
+  for x in range(0,fingerprintsCount):
+    os = obj.WEBSERVER.fingerprints.fingerprint[x]['name']
+    testsCount = len(obj.WEBSERVER.fingerprints.fingerprint[x].webserver_tests)
+    test = {}
+    for y in range(0,testsCount):
+      test = obj.WEBSERVER.fingerprints.fingerprint[x].webserver_tests.test[y]
+      if test is None:  #if testsCount = 1, then untangle doesn't allow us to iterate through it
+        test = obj.WEBSERVER.fingerprints.fingerprint[x].webserver_tests.test
+      matchtype = test['matchtype']
+      webserver = test['webserver']
+      weight = test['weight']
+      if matchtype == 'exact':
+        if webserver in serverExactList:
+          oldValue = serverExactList.get(webserver)
+          serverExactList[webserver] = oldValue + '|' + os + ':' + weight
+        else:
+          serverExactList[webserver] = os + ':' + weight
+      else:
+        if webserver in serverPartialList:
+          oldValue = serverPartialList.get(webserver)
+          serverPartialList[webserver] = oldValue + '|' + os + ':' + weight
+        else:
+          serverPartialList[webserver] = os + ':' + weight
+
+  return [serverExactList, serverPartialList]
 
 
 
@@ -84,13 +157,37 @@ def BuildHTTPUserAgentFingerprintFiles():
   return [useragentExactList, useragentPartialList]
 
 
+def httpServerFingerprintLookup(exactList, partialList, value):
+  #same as DHCP one, may be able to look at combining in the future?
+  exactValue = ''
+  partialValue = ''
+
+  if value in exactList:
+    exactValue = exactList.get(value)
+
+  for key, val in partialList.items():
+    if value.find(key) > -1:
+      partialValue = partialValue + '|' + val
+
+  if partialValue.startswith('|'):
+    partialValue = partialValue[1:]
+  if partialValue.endswith('|'):
+    partialValue = partialValue[:-1]
+
+  fingerprint = exactValue + '|' + partialValue
+  if fingerprint.startswith('|'):
+    fingerprint = fingerprint[1:]
+  if fingerprint.endswith('|'):
+    fingerprint = fingerprint[:-1]
+
+  return fingerprint
+
 
 
 def httpUserAgentFingerprintLookup(exactList, partialList, value):
   #same as DHCP one, may be able to look at combining in the future?
   exactValue = ''
   partialValue = ''
-  print (value)
 
   if value in exactList:
     exactValue = exactList.get(value)
